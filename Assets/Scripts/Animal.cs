8cb5a3be004096a2b9ae0235a54275af4ac3d1da
Kemal <kemal_zahid@hotmail.com>
2020-10-21 16:19:59 +0300

    patch1

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    public int speed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1) * speed;
+    }
+
+    void OnBecameInvisible()
+    { Destroy(gameObject); }
+}
---
+++ Assets/Scripts/WolfMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfMovement : MonoBehaviour {
+
+    public float toPosX;
+    public float toPosY;
+    public float time;
+
+	// Use this for initialization
+	void Start () {
+        iTween.MoveTo(this.gameObject, iTween.Hash(
+                                                    "x", toPosX,
+                                                    "y", toPosY,
+                                                    "time", time,
+                                                    "easetype", "linear",
+                                                    "looptype", "pingPong",
+                                                    "onComplete", "FlipSprite"));
+
+        if(this.gameObject.transform.position.x > toPosX)
+        {
+            FlipSprite();
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FlipSprite()
+    {
+        if(this.gameObject.GetComponent<SpriteRenderer>().flipX == false)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    { this.gameObject.GetComponent<AudioSource>().Play(); }
+}
